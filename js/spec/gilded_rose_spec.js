dfa11ba0efb3caf47d706bfb98e65ab59f5f6acd
Ensar <ensar_mehmedovic@hotmail.com>
2025-06-27 09:13:25 +0200

    Die Qualität steigt nie über 50 (chatgpt)

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -40,6 +40,12 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(12); // Qualität +2
   });
 
+  it("never increases quality above 50", function() {
+    items = [ new Item("Aged Brie", 5, 50) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50); // bleibt bei 50
+  });
+
 
 
 });
