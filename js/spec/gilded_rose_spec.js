4a879fcbe989fcde5865c4ea35d9be631d011b7f
Ensar <ensar_mehmedovic@hotmail.com>
2025-06-27 08:50:04 +0200

    sell_in und quality bei normalen items sinken jeden tag um 1 (chatgpt)

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,19 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+  /*it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
     expect(items[0].name).toEqual("fixme");
+  }); */
+
+  it("reduces sell_in and quality by 1 for a normal item", function() {
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);  
   });
+
+
+
 
 });
