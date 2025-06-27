5afb06ebeda05014643a64ba21db6ff44b27332c
Ensar <ensar_mehmedovic@hotmail.com>
2025-06-27 09:20:38 +0200

    Sulfuras verändert sich nicht (chatgpt)

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -46,6 +46,13 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(50); // bleibt bei 50
   });
 
+    it("Sulfuras never decreases in sell_in or quality", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+  });
+
 
 
 });
