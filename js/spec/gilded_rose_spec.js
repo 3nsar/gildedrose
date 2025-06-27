edc3384b003d9f12c61a181df7b3351520ef8be0
Ensar <ensar_mehmedovic@hotmail.com>
2025-06-27 08:59:27 +0200

    quality verschlechtert sich um das doppelte nach sell_in (chatgpt)

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -13,6 +13,13 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(19);  
   });
 
+    it("quality degrades twice as fast after sell_in <= 0", function() {
+    items = [ new Item("Elixir of the Mongoose", 0, 10) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(8);
+  });
+
 
 
 
